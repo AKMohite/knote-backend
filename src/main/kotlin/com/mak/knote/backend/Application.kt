de00f67581c596b7c0934ef00baa6296412c7f41
Ashish Mohite <ashishmohite1996@gmail.com>
2023-05-09 12:32:45 +0530

    initial commit

---
+++ src/main/kotlin/com/mak/knote/backend/Application.kt
@@ -0,0 +1,16 @@
+package com.mak.knote.backend
+
+import com.mak.knote.backend.plugins.configureRouting
+import com.mak.knote.backend.plugins.configureSecurity
+import com.mak.knote.backend.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/test/kotlin/com/mak/knote/backend/ApplicationTest.kt
@@ -0,0 +1,24 @@
+package com.mak
+
+import com.mak.knote.backend.plugins.configureRouting
+
+-knote
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
